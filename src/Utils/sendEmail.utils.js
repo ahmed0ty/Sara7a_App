9e3b5258a4d5c58299dbcae78ce29b2916d007be
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-03-07 15:03:04 +0200

    first commit

--- src/Utils/sendEmail.utils.js
+++ src/Utils/sendEmail.utils.js
@@ -1,48 +1,37 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({
   to = "",
-  subject = "",
-  text = "",
+  subject = "Sara7a Application",
   html = "",
 }) {
 
-  console.log("STEP 1: sendEmail called");
-  console.log("EMAIL:", process.env.EMAIL);
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.APP_PASSWORD,
-    },
-  });
-
-  console.log("STEP 2: transporter created");
-
   try {
 
-    console.log("STEP 3: before sendMail");
-
-    const info = await transporter.sendMail({
-      from: `Sara7a 👻 <${process.env.EMAIL}>`,
-      to,
-      subject,
-      text,
-      html,
+    const { data, error } = await resend.emails.send({
+      from: "Sara7a 👻 <onboarding@resend.dev>",
+      to: to,
+      subject: subject,
+      html: html,
     });
 
-    console.log("STEP 4: message sent", info.messageId);
+    if (error) {
+      console.log("Email Error:", error);
+      return;
+    }
 
-  } catch (error) {
+    console.log("Email sent successfully:", data);
 
-    console.log("STEP ERROR:", error);
-
+  } catch (err) {
+    console.log("Resend Error:", err);
   }
+
 }
 
 export const emailSubject = {
-  confirmEmail: "confirm your email",
-  resetPassword: "reset your password",
-  welcome: "welcome to route academy",
+  confirmEmail: "Confirm your email",
+  resetPassword: "Reset your password",
+  welcome: "Welcome to Sara7a",
 };
