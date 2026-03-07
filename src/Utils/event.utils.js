995c1965382cec527573605f96c5d6eb75b0374f
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-03-07 12:20:30 +0200

    third commit

--- src/Utils/event.utils.js
+++ src/Utils/event.utils.js
@@ -1,41 +1,29 @@
-
 import { EventEmitter } from "node:events";
 import { emailSubject, sendEmail } from "../Utils/sendEmail.utils.js";
 import { template } from "./email/generateHTML.js";
 
-
-
 export const emailEvent = new EventEmitter();
 
 emailEvent.on("confirmEmail", async (data) => {
-  await sendEmail({
-    to: data.to,
-    html: template(data.otp,data.firstName),
-    subject: emailSubject.confirmEmail,
-  });
+  try {
+    await sendEmail({
+      to: data.to,
+      html: template(data.otp, data.firstName),
+      subject: emailSubject.confirmEmail,
+    });
+  } catch (error) {
+    console.log("Email Error:", error);
+  }
 });
-
 
 emailEvent.on("forgetpassword", async (data) => {
-  await sendEmail({
-    to: data.to,
-    html: template(data.otp,data.firstName,emailSubject.resetPassword),
-    subject: emailSubject.resetPassword,
-  });
+  try {
+    await sendEmail({
+      to: data.to,
+      html: template(data.otp, data.firstName, emailSubject.resetPassword),
+      subject: emailSubject.resetPassword,
+    });
+  } catch (error) {
+    console.log("Email Error:", error);
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
